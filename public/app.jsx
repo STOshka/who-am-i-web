1479ee521c9015ed00d850423c8f15c82dbbd458
xdghcnt <xd-mail@ya.ru>
2020-05-04 17:21:56 +0300

    Public rooms

--- public/app.jsx
+++ public/app.jsx
@@ -357,8 +357,9 @@ class Game extends React.Component {
                         </div>
                         <div className="host-controls" onTouchStart={(e) => e.target.focus()}>
                             <div className="side-buttons">
-                                <i onClick={() => this.socket.emit("set-room-mode", false)}
-                                   className="material-icons exit settings-button">store</i>
+                                {this.state.userId === this.state.hostId ?
+                                    <i onClick={() => this.socket.emit("set-room-mode", false)}
+                                       className="material-icons exit settings-button">store</i> : ""}
                                 {(isHost || data.rolesLocked) ? (data.rolesLocked
                                     ? (<i onClick={() => this.handleToggleRoleLockClick()}
                                           className="material-icons-outlined start-game settings-button">label_off</i>)
